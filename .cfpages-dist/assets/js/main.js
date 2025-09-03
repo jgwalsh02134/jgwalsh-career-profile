fd5a83e40831920224a388ede52fc6a057eceb87
JG Walsh <wombats.simmers-0e@icloud.com>
2025-09-02 20:28:54 -0400

    Ignore .cfpages-dist build output

--- .cfpages-dist/assets/js/main.js
+++
@@ -1,1 +0,0 @@
-console.log("Pages static asset loaded.");
--- .cfpages-dist/functions/api/health.js
+++
@@ -1,6 +0,0 @@
-export const onRequest = () => {
-  return new Response(JSON.stringify({ ok: true, ts: Date.now() }), {
-    headers: { "content-type": "application/json" },
-    status: 200,
-  });
-};
